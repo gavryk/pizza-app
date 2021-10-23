471dea8df95c2f5a6a90030f0e821895795bddc0
gvozdyk <gvozdyk23@gmail.com>
2021-10-23 12:13:38 +0300

    comeback

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,15 @@ import logo from '../../assets/img/pizza-logo.png';
 const Header = () => {
     return (
         <div className={style.header}>
-            <div className={style.headerWrapper}>
-                <div className={style.logo}>
-                    <img src={ logo } alt=""/>
-                    <h3>React Pizza!</h3>
+            <div className={`${style.headerWrapper} d-flex justify-content-between align-items-center`}>
+                <div className={`${style.logo} col-6`}>
+                    <img src={ logo } alt="Logo"/>
+                    <h4>REACT PIZZA</h4>
+                </div>
+
+                <div className={`${style.headerButton} col-2`}>
+                    <button>2000</button>
+                    <button>Cart</button>
                 </div>
             </div>
         </div>
