6521b975fb26851f4bb56074ebb41bcc6aa67255
gvozdyk <gvozdyk23@gmail.com>
2021-10-28 23:38:53 +0300

    sort popup

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,35 +1,45 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import style from './Sort.module.scss';
 
 const Sort = () => {
     const [sortBy, setSortBy] = useState('Popular');
     const [toggleSort, setToggleSort] = useState(false);
+    const sortRef = useRef();
     let sortList = ['Popular', 'Price', 'Alphabet'];
 
-    const openSortList = () => {
+    const toggleVisibleList = () => {
         setToggleSort(!toggleSort);
     }
+
     const selectSort = (el) => {
         setSortBy(el.target.innerText);
-        setToggleSort(!toggleSort);
+        // setToggleSort(!toggleSort);
     }
+
+    const clickOffSortPopup = (e) => {
+
+    }
+
+    useEffect(() => {
+        document.body.addEventListener('click', clickOffSortPopup);
+    }, []);
 
     return (
         <div className={ style.dropdown }>
             <div className="sort__label">
-                <span onClick={ openSortList } className="btn dropdown-header" id="dropdownMenuButton1"
+                <span onClick={ toggleVisibleList } className="btn dropdown-header" id="dropdownMenuButton1"
                         data-bs-toggle="dropdown" aria-expanded="false">
                     <b className='mx-2'>Sort By:</b>
-                    { sortBy }
+                    <span className={ style.selectSort }>{ sortBy }</span>
                 </span>
             </div>
             <ul id='sort-dropdown-menu'
                 className={`${style.dropdownMenu} ${ toggleSort && style.visibleSortList } `}
                 aria-labelledby="dropdownMenuLink">
                 {
-                    sortList.map(function(el) {
+                    sortList.map(function(el, index) {
                         return(
-                            <li onClick={ selectSort } className="dropdown-item">
+                            <li key={`${el}_${index}`} onClick={ selectSort } className="dropdown-item">
                                 { el }
                             </li>
                         )
--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,3 @@ export { default as Header } from './Header/Header';
 export { default as Categories } from './Categories/Categories';
 export { default as Button } from './Button/Button';
 export { default as Sort } from './Sort/Sort';
-
-
-
-// import Header from "./Header/Header";
-// import Categories from "./Categories/Categories";
-//
-// export {
-//     Header,
-//     Categories
-// }
