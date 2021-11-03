ed75851a462c51c26cb5a7994d5928ebd3445e95
gvozdyk <gvozdyk23@gmail.com>
2021-11-04 00:54:13 +0200

    add pizza block

--- src/components/Pizza/Pizza.jsx
+++ src/components/Pizza/Pizza.jsx
@@ -2,16 +2,31 @@ import React from "react";
 import style from './Pizza.module.scss'
 
 const Pizza = ({ imgUrl, name }) => {
+    const availableTypes = ['thin', 'traditional'];
+    const availableSizes = [26, 30, 40];
+
     return (
         <div className={ style.pizzaBlock }>
             <img className={`${ style.pizzaBlockImage } mb-2`} src={ imgUrl } alt="Pizza" />
             <h4 className={ style.pizzaBlockTitle }>{ name }</h4>
             <div className="pizza-block__selector">
                 <ul>
-
+                    {availableTypes.map((type, index) => (
+                        <li
+                            key={type}
+                        >
+                            {type}
+                        </li>
+                    ))}
                 </ul>
                 <ul>
-
+                    {availableSizes.map((size, index) => (
+                        <li
+                            key={size}
+                        >
+                            {size} sm
+                        </li>
+                    ))}
                 </ul>
             </div>
             <div className="pizza-block__bottom">
