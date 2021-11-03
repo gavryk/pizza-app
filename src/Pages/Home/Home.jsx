8364f74839063f598a924d2e633dc52a768d8a8c
gvozdyk <gvozdyk23@gmail.com>
2021-11-03 23:52:49 +0200

    create fake db (db.json)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,12 +5,17 @@ import {Categories, Sort} from "../../components";
 const Home = ({ categories, sortList }) => {
 
     return (
-        <div className={`${ style.content__top } d-flex justify-content-between`}>
-            <div className='col-8'>
-                <Categories catClickItem={ (name) => console.log(name) } items={ categories }/>
+        <div className={style.homeContent}>
+            <div className={`${ style.content__top } d-flex justify-content-between`}>
+                <div className='col-8'>
+                    <Categories catClickItem={ (name) => console.log(name) } items={ categories }/>
+                </div>
+                <div className="col-3 d-flex align-items-center justify-content-center">
+                    <Sort sortList={ sortList } />
+                </div>
             </div>
-            <div className="col-3 d-flex align-items-center justify-content-center">
-                <Sort sortList={ sortList } />
+            <div className={`${style.mainContent} pt-1 pb-3 px-4`}>
+                <h2>All Pizza</h2>
             </div>
         </div>
     )
