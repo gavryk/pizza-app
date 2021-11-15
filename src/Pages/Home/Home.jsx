b3cb95b68e634b7e4eb8c9ca4b762619f7f7f4ab
Oleg <gvozdyk23@gmail.com>
2021-11-15 14:48:33 +0200

    redux hooks (useSelector, useDispatch) it's f***ing amazing, hooks - the best

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,17 @@ import style from './Home.module.scss';
 import { Categories, Sort, Pizza } from "../../components";
 import axios from "axios";
 import { setPizzas } from "../../redux/actions/pizzas";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-const Home = ({ categories, sortList, setPizzas, ...props }) => {
+
+const Home = ({ categories, sortList }) => {
+    const dispatch = useDispatch();
+    const pizzas = useSelector((state) => state.pizzas.items);
 
     useEffect(() => {
         axios.get('http://localhost:3000/db.json')
             .then(({ data }) => {
-                setPizzas(data.pizzas);
+                dispatch(setPizzas(data.pizzas));
             });
     }, []);
 
@@ -26,10 +29,10 @@ const Home = ({ categories, sortList, setPizzas, ...props }) => {
             </div>
             <div className={`${style.mainContent} pt-1 pb-3 px-4`}>
                 <h2 className=''>All Pizza</h2>
-                {   props.items &&
+                {   pizzas &&
                     <div className={`${style.pizzaWrapper} py-4`}>
                         {
-                            props.items.map((item, index) => {
+                            pizzas.map((item, index) => {
                                 return(
                                     <Pizza
                                         key={item.id}
@@ -45,15 +48,62 @@ const Home = ({ categories, sortList, setPizzas, ...props }) => {
     )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        items: state.pizzas.items
-    }
-}
+export default Home;
 
-const mapDispatchToProps = {
-    setPizzas
-}
+
+//NOT ACTUAL, now i use redux hooks
+
+// class Home extends React.Component {
+//     componentDidMount() {
+//         axios.get('http://localhost:3000/db.json')
+//         .then(({ data }) => {
+//             this.props.setPizzas(data.pizzas);
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div className={style.homeContent}>
+//                 <div className={`${ style.content__top } d-flex justify-content-between`}>
+//                     <div className='col-8'>
+//                         <Categories items={ categories }/>
+//                     </div>
+//                     <div className="col-3 d-flex align-items-center justify-content-center">
+//                         <Sort sortList={ sortList } />
+//                     </div>
+//                 </div>
+//                 <div className={`${style.mainContent} pt-1 pb-3 px-4`}>
+//                     <h2 className=''>All Pizza</h2>
+//                     {   this.props.items &&
+//                         <div className={`${style.pizzaWrapper} py-4`}>
+//                             {
+//                                 this.props.items.map((item, index) => {
+//                                     return(
+//                                         <Pizza
+//                                             key={item.id}
+//                                             { ...item }
+//                                         />
+//                                     )
+//                                 })
+//                             }
+//                         </div>
+//                     }
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// const mapStateToProps = (state) => {
+//     return {
+//         items: state.pizzas.items,
+//         filters: state.filters
+//     }
+// }
+
+// const mapDispatchToProps = {
+//     setPizzas
+// }
 
 // const mapDispatchToProps = (dispatch) => {
 //     return {
@@ -63,4 +113,4 @@ const mapDispatchToProps = {
 //     };
 // }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+// export default connect(mapStateToProps, mapDispatchToProps)(Home);
