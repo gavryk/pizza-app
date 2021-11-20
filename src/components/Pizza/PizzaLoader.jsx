cee57378e33ae2be7a7fd890168f81eefefca763
gvozdyk <gvozdyk23@gmail.com>
2021-11-20 02:07:41 +0200

    sort & filter query

--- src/components/Pizza/PizzaLoader.jsx
+++ src/components/Pizza/PizzaLoader.jsx
@@ -12,10 +12,10 @@ const PizzaLoader = () => {
             foregroundColor="#ecebeb"
         >
             <circle cx="164" cy="131" r="131" /> 
-            <rect x="30" y="285" rx="0" ry="0" width="269" height="19" /> 
-            <rect x="32" y="321" rx="10" ry="10" width="268" height="76" /> 
-            <rect x="166" y="416" rx="25" ry="25" width="132" height="45" /> 
-            <rect x="32" y="424" rx="0" ry="0" width="84" height="30" />
+            <rect x="58" y="284" rx="0" ry="0" width="218" height="19" /> 
+            <rect x="0" y="322" rx="10" ry="10" width="337" height="76" /> 
+            <rect x="186" y="415" rx="25" ry="25" width="150" height="45" /> 
+            <rect x="5" y="423" rx="0" ry="0" width="120" height="30" />
         </ContentLoader>
     )
 }
