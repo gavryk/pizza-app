bb7db1f804103032e8332852adea2fb46aa4fbf0
gvozdyk <gvozdyk23@gmail.com>
2021-11-22 21:42:13 +0200

    fix bugs

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -5,7 +5,7 @@ export const fetchPizzas = (category, sortBy) => {
     let catQuery = category !== null ? `category=${category}&` : '';
     return dispatch => {
         dispatch(setLoading(false));
-        axios.get(`https://pizzas-store-app.herokuapp.com/api?${catQuery}_sort=${sortBy.type}&order=${sortBy.order}`)
+        axios.get(`https://localhost:3001/pizzas?${catQuery}_sort=${sortBy.type}&order=${sortBy.order}`)
             .then(({ data }) => {
                 dispatch(setPizzas(data));
                 dispatch(setLoading(true));
